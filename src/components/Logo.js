8141d4ded49cf16a63f7870d216ff50f66004417
Basith Ahamad <basith.ahamad@hotmail.com>
2026-03-11 15:55:11 +0530

    Update

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,28 +1,15 @@
 import React from "react";
+import logoImage from "../images/logo-ba.png";
 
 const Logo = ({ className = "" }) => {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 56 32"
-      preserveAspectRatio="xMidYMid meet"
+    <img
+      src={logoImage}
+      alt="BA"
       className={className}
-      style={{ width: "100%", height: "auto" }}
+      style={{ width: "100%", height: "auto", display: "block" }}
       aria-hidden="true"
-    >
-      <text
-        x="28"
-        y="22"
-        textAnchor="middle"
-        fill="currentColor"
-        fontFamily="system-ui, -apple-system, 'Segoe UI', sans-serif"
-        fontSize="24"
-        fontWeight="700"
-        letterSpacing="0.05em"
-      >
-        BA
-      </text>
-    </svg>
+    />
   );
 };
 
